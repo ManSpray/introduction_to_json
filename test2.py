380669af1e173f2080f21c9173c4776270a787c1
Mantas <64923788+ManSpray@users.noreply.github.com>
2020-06-23 09:55:11 +0300

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,9 +1,24 @@
 import requests
 import json
 from urllib.request import urlopen
-d = "2019-09-25"
 
-with urlopen('https://api.exchangeratesapi.io/' + d ) as response:
+print("")
+pd = input("Įveskite pirmąją datą: ")
+ad = input("Įveskite antrąją datą: ")
+val = input("Įveskite norimą valiutą: ")
+
+with urlopen('https://api.exchangeratesapi.io/' + pd ) as response:
+    source = response.read()
+
+print(source)
+data = json.loads(source)
+
+#print(len(data['rates']))
+
+print(pd, val, "valiutos kursas: ", data['rates'][val])
+first = data['rates'][val]
+
+with urlopen('https://api.exchangeratesapi.io/' + ad ) as response:
     source = response.read()
 
 #print(source)
@@ -11,20 +26,9 @@ data = json.loads(source)
 
 #print(len(data['rates']))
 
-print(data['rates']['CAD'])
-first = data['rates']['CAD']
-d = "2020-05-14"
-with urlopen('https://api.exchangeratesapi.io/' + d ) as response:
-    source = response.read()
-
-#print(source)
-data = json.loads(source)
-
-#print(len(data['rates']))
-
-print(data['rates']['CAD'])
-second = data['rates']['CAD']
-answer = second - first
-difp = 100 - (first * 100 / second) 
-print(answer)
-print(difp)
+print(ad, val, "valiutos kursas: ", data['rates'][val])
+second = data['rates'][val]
+answer = first - second
+difp = 100 - (second * 100 / first) 
+print("Valiutos skirtumas: ", answer)
+print("Valiutos skirtumas procentais: ", difp)
