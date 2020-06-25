11961e635315eab5380940014ebf42145c05920c
Mantas <64923788+ManSpray@users.noreply.github.com>
2020-06-25 13:46:02 +0300

    Add files via upload

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ import json
 from urllib.request import urlopen
 
 failas = open("valiutos.txt", "r")
-print(failas.readable())
+#print(failas.readable())
 #print(failas.read())
 pd = failas.readline() #input("Įveskite pirmąją datą: ")
 ad = failas.readline() # input("Įveskite antrąją datą: ")
@@ -13,12 +13,14 @@ failas.close()
 with urlopen('https://api.exchangeratesapi.io/' + pd ) as response:
     source = response.read()
 
-print(source)
+#print(source)
 data = json.loads(source)
 
 #print(len(data['rates']))
 ats1 = str (data['rates'][val])
 rez = open("rezultatai.txt", "w")
+rez.write(pd)
+rez.write('Valiutos kursas:   ')
 rez.write(ats1)
 rez.write('\n')
 #print(pd, val, "valiutos kursas: ", data['rates'][val])
@@ -31,15 +33,18 @@ with urlopen('https://api.exchangeratesapi.io/' + ad ) as response:
 data = json.loads(source)
 
 #print(len(data['rates']))
+rez.write(ad)
+rez.write('Valiutos kursas:  ')
 ats2 = str (data['rates'][val])
 rez.write(ats2)
 rez.write('\n')
-#print(ad, val, "valiutos kursas: ", data['rates'][val])
+print(ad, val, "valiutos kursas: ", data['rates'][val])
 second = data['rates'][val]
 answer = str (first - second)
 difp = str (100 - (second * 100 / first))
+rez.write("Valiutos skirtumas:   ")
 rez.write(answer)
-rez.write('\n')
+rez.write('\nValiutos skirtumas procentais:  ')
 rez.write(difp)
 rez.write('\n')
 rez.close()
